99eb47113a09a483796eb0ae8f03b778abb4dbac
dahm <dahm@8be7c0d5-fe64-45a8-b24d-1780f0814b93>
2011-08-31 09:54:19 +0000

    bugfix: swallowed exception
    git-svn-id: https://jbosscc-needle.svn.sourceforge.net/svnroot/jbosscc-needle/trunk/jbosscc-needle@95 8be7c0d5-fe64-45a8-b24d-1780f0814b93

--- src/main/java/de/akquinet/jbosscc/needle/NeedleTestcase.java
+++ src/main/java/de/akquinet/jbosscc/needle/NeedleTestcase.java
@@ -20,156 +20,145 @@ import de.akquinet.jbosscc.needle.mock.MockProvider;
 import de.akquinet.jbosscc.needle.reflection.ReflectionUtil;
 
 public class NeedleTestcase {
+  private static final Logger LOG = LoggerFactory.getLogger(NeedleTestcase.class);
 
-	private static final Logger LOG = LoggerFactory.getLogger(NeedleTestcase.class);
+  private static final InjectionIntoAnnotationProcessor INJECTION_INTO_ANNOTATION_PROCESSOR = new InjectionIntoAnnotationProcessor();
 
-	private static final InjectionIntoAnnotationProcessor INJECTION_INTO_ANNOTATION_PROCESSOR = new InjectionIntoAnnotationProcessor();
+  private final List<InjectionProvider> injectionProviderList = new ArrayList<InjectionProvider>();
 
-	private List<InjectionProvider> injectionProviderList = new ArrayList<InjectionProvider>();
+  private final InjectionConfiguration configuration = new InjectionConfiguration();
+  private final Map<Object, Object> injectedObjectMap = new HashMap<Object, Object>();
 
-	private InjectionConfiguration configuration = new InjectionConfiguration();
-	private final Map<Object, Object> injectedObjectMap = new HashMap<Object, Object>();
+  public NeedleTestcase(final InjectionProvider... injectionProvider) {
+    assignInejctionProvider(injectionProvider);
+  }
 
-	public NeedleTestcase(final InjectionProvider... injectionProvider) {
-		super();
-		assignInejctionProvider(injectionProvider);
-	}
+  public NeedleTestcase(final DatabaseTestcase databaseTestcase, final InjectionProvider... injectionProvider) {
+    this(injectionProvider);
+    addEntityManagerProvider(databaseTestcase);
+  }
 
-	public NeedleTestcase(final DatabaseTestcase databaseTestcase, final InjectionProvider... injectionProvider) {
+  private void addEntityManagerProvider(final DatabaseTestcase databaseTestcase) {
+    final InjectionProvider entityManagerProvider = new EntityManagerProvider(databaseTestcase);
+    final InjectionProvider entityManagerFactoryProvider = new EntityManagerFactoryProvider(databaseTestcase);
 
-		this(injectionProvider);
-		addEntityManagerProvider(databaseTestcase);
+    assignInejctionProvider(entityManagerProvider, entityManagerFactoryProvider);
+  }
 
-	}
+  protected final void assignInejctionProvider(final InjectionProvider... injectionProvider) {
+    for (final InjectionProvider provider : injectionProvider) {
+      injectionProviderList.add(0, provider);
+    }
+  }
 
-	private void addEntityManagerProvider(final DatabaseTestcase databaseTestcase) {
-		final InjectionProvider entityManagerProvider = new EntityManagerProvider(databaseTestcase);
-		final InjectionProvider entityManagerFactoryProvider = new EntityManagerFactoryProvider(databaseTestcase);
+  protected final void initTestcase(final Object test) throws Exception {
+    LOG.info("init testcase");
 
-		assignInejctionProvider(entityManagerProvider, entityManagerFactoryProvider);
+    injectedObjectMap.clear();
 
-	}
+    final List<Field> fields = ReflectionUtil.getAllFieldsWithAnnotation(test, ObjectUnderTest.class);
 
-	protected final void assignInejctionProvider(final InjectionProvider... injectionProvider) {
-		for (InjectionProvider provider : injectionProvider) {
-			injectionProviderList.add(0, provider);
-		}
-	}
+    LOG.debug("found fields {}", fields);
 
-	protected final void initTestcase(Object test) throws Exception {
+    for (final Field field : fields) {
+      LOG.info("found field {}", field.getName());
+      try {
+        final Object instance = setInstanceIfNotNull(field, test);
+        initInstance(instance);
+      } catch (final InstantiationException e) {
+        LOG.error(e.getMessage(), e);
+      } catch (final IllegalAccessException e) {
+        LOG.error(e.getMessage(), e);
+      }
+    }
 
-		LOG.info("init testcase");
+    handleInjectInto(test);
+  }
 
-		injectedObjectMap.clear();
+  private void handleInjectInto(final Object test) {
+    INJECTION_INTO_ANNOTATION_PROCESSOR.process(test);
+  }
 
-		final List<Field> fields = ReflectionUtil.getAllFieldsWithAnnotation(test, ObjectUnderTest.class);
+  protected final void initInstance(final Object instance) {
+    final List<Field> fields = ReflectionUtil.getAllFields(instance.getClass());
+    fieldIteration: for (final Field field : fields) {
 
-		LOG.debug("found fields {}", fields);
+      // Custom provider
+      for (final InjectionProvider provider : injectionProviderList) {
+        if (handleInjection(instance, field, provider)) {
+          continue fieldIteration;
+        }
+      }
 
-		for (Field field : fields) {
-			LOG.info("found field {}", field.getName());
-			try {
-				Object instance = setInstanceIfNotNull(field, test);
-				initInstance(instance);
-			} catch (InstantiationException e) {
-				LOG.error(e.getMessage(), e);
-			} catch (IllegalAccessException e) {
-				LOG.error(e.getMessage(), e);
-			}
-		}
+      // Default provider from configuration
+      for (final InjectionProvider provider : configuration.getInjectionProvider()) {
+        if (handleInjection(instance, field, provider)) {
+          continue fieldIteration;
+        }
+      }
+    }
+  }
 
-		handleInjectInto(test);
+  private boolean handleInjection(final Object instance, final Field field, final InjectionProvider provider) {
+    if (provider.verify(field)) {
+      final Object object = provider.get(field.getType());
+      setFieldValue(instance, field, object);
+      injectedObjectMap.put(provider.getKey(field), object);
+      return true;
+    }
 
-	}
+    return false;
+  }
 
-	private void handleInjectInto(Object test) {
-		INJECTION_INTO_ANNOTATION_PROCESSOR.process(test);
-	}
+  private void setFieldValue(final Object instance, final Field field, final Object fieldValue) {
+    try {
+      ReflectionUtil.setField(field, instance, fieldValue);
+    } catch (final Exception e) {
+      LOG.error(e.getMessage(), e);
+    }
+  }
 
-	protected final void initInstance(Object instance) {
+  private Object setInstanceIfNotNull(final Field field, final Object test) throws Exception {
 
-		final List<Field> fields = ReflectionUtil.getAllFields(instance.getClass());
-		fieldIteration: for (Field field : fields) {
+    Object instance = ReflectionUtil.getFieldValue(test, field);
 
-			// Custom provider
-			for (InjectionProvider provider : injectionProviderList) {
-				if (handleInjection(instance, field, provider)) {
-					continue fieldIteration;
-				}
-			}
+    if (instance == null) {
+      final ObjectUnderTest annotation = field.getAnnotation(ObjectUnderTest.class);
 
-			// Default provider from configuration
-			for (InjectionProvider provider : configuration.getInjectionProvider()) {
-				if (handleInjection(instance, field, provider)) {
-					continue fieldIteration;
-				}
+      final Class<?> implementation = annotation.implementation() != Void.class ? annotation.implementation() : field.getType();
 
-			}
-		}
+      if (implementation.isInterface()) {
+        throw new ObjectUnderTestInstantiationException("could not create an instance of object under test " + implementation
+            + ", no implementation class configured");
+      }
 
-	}
+      try {
+        implementation.getConstructor();
+      } catch (final NoSuchMethodException e) {
+        throw new ObjectUnderTestInstantiationException("could not create an instance of object under test " + implementation
+            + ",implementation has no public no-arguments constructor", e);
+      }
 
-	private boolean handleInjection(Object instance, Field field, InjectionProvider provider) {
-		if (provider.verify(field)) {
-			Object object = provider.get(field.getType());
-			setFieldValue(instance, field, object);
-			injectedObjectMap.put(provider.getKey(field), object);
-			return true;
-		}
+      instance = implementation.newInstance();
 
-		return false;
-	}
+      try {
+        ReflectionUtil.setField(field, test, instance);
+      } catch (final Exception e) {
+        throw new ObjectUnderTestInstantiationException(e);
+      }
+    }
 
-	private void setFieldValue(Object instance, Field field, Object fieldValue) {
-		try {
-			ReflectionUtil.setField(field, instance, fieldValue);
-		} catch (Exception e) {
-			LOG.error(e.getMessage(), e);
-		}
-	}
+    return instance;
 
-	private Object setInstanceIfNotNull(Field field, Object test) throws Exception {
+  }
 
-		Object instance = ReflectionUtil.getFieldValue(test, field);
+  @SuppressWarnings("unchecked")
+  public <X> X getInjectedObject(final Object key) {
+    return (X) injectedObjectMap.get(key);
+  }
 
-		if (instance == null) {
-			ObjectUnderTest annotation = field.getAnnotation(ObjectUnderTest.class);
-
-			final Class<?> implementation = annotation.implementation() != Void.class ? annotation.implementation()
-			        : field.getType();
-
-			if (implementation.isInterface()) {
-				throw new ObjectUnderTestInstantiationException("could not create an instance of object under test "
-				        + implementation + ", no implementation class configured");
-			}
-
-			try {
-				implementation.getConstructor();
-			} catch (NoSuchMethodException e) {
-				throw new ObjectUnderTestInstantiationException("could not create an instance of object under test "
-				        + implementation + ",implementation has no public no-arguments constructor", e);
-			}
-
-			instance = implementation.newInstance();
-
-			try {
-				ReflectionUtil.setField(field, test, instance);
-			} catch (Exception e) {
-				throw new ObjectUnderTestInstantiationException(e);
-			}
-		}
-
-		return instance;
-
-	}
-
-	@SuppressWarnings("unchecked")
-	public <X> X getInjectedObject(final Object key) {
-		return (X) injectedObjectMap.get(key);
-	}
-
-	public <X extends MockProvider> X getMockProvider() {
-		return configuration.getMockProvider();
-	}
+  public <X extends MockProvider> X getMockProvider() {
+    return configuration.getMockProvider();
+  }
 
 }
--- src/main/java/de/akquinet/jbosscc/needle/junit/NeedleRule.java
+++ src/main/java/de/akquinet/jbosscc/needle/junit/NeedleRule.java
@@ -1,7 +1,5 @@
 package de.akquinet.jbosscc.needle.junit;
 
-import junit.framework.AssertionFailedError;
-
 import org.junit.rules.MethodRule;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
@@ -12,23 +10,21 @@ import de.akquinet.jbosscc.needle.injection.InjectionProvider;
 
 public class NeedleRule extends NeedleTestcase implements MethodRule {
 
+  public NeedleRule(final DatabaseTestcase databaseTestcase, final InjectionProvider... injectionProvider) {
+    super(databaseTestcase, injectionProvider);
+  }
 
-	public NeedleRule(DatabaseTestcase databaseTestcase, InjectionProvider... injectionProvider) {
-	    super(databaseTestcase, injectionProvider);
-    }
+  public NeedleRule(final InjectionProvider... injectionProvider) {
+    super(injectionProvider);
+  }
 
-	public NeedleRule(InjectionProvider... injectionProvider) {
-		super(injectionProvider);
-    }
-
-	@Override
-	public Statement apply(Statement base, FrameworkMethod method, Object target) {
-		try {
-			initTestcase(target);
-		} catch (Exception e) {
-			throw new AssertionFailedError(e.getMessage());
-		}
-		return base;
-	}
-
+  @Override
+  public Statement apply(final Statement base, final FrameworkMethod method, final Object target) {
+    return new Statement() {
+      @Override
+      public void evaluate() throws Throwable {
+        initTestcase(target);
+      }
+    };
+  }
 }
