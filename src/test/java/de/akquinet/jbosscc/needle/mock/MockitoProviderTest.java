5fb9daafca4cf15693754f0c46b74cf238cc70d4
hwilming <hwilming@8be7c0d5-fe64-45a8-b24d-1780f0814b93>
2011-09-15 18:18:01 +0000

    Mockito test
    git-svn-id: https://jbosscc-needle.svn.sourceforge.net/svnroot/jbosscc-needle/trunk/jbosscc-needle@114 8be7c0d5-fe64-45a8-b24d-1780f0814b93

---
+++ src/test/java/de/akquinet/jbosscc/needle/mock/MockitoProviderTest.java
@@ -0,0 +1,34 @@
+package de.akquinet.jbosscc.needle.mock;
+
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class MockitoProviderTest {
+
+	private MockitoProvider mockitoProvider = new MockitoProvider();
+
+	@Test
+	public void testCreateMockComponent() throws Exception {
+		@SuppressWarnings("unchecked")
+		Map<String, String> mapMock = mockitoProvider.createMockComponent(Map.class);
+
+		String key = "key";
+		String value = "value";
+
+		Mockito.when(mapMock.get(key)).thenReturn(value);
+
+		Assert.assertEquals(value, mapMock.get(key));
+
+	}
+
+	@Test
+	public void testCreateMockComponent_Final() throws Exception {
+		// TODO
+
+	}
+
+}
