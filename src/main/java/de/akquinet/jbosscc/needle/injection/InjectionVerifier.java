202fda83cae92c4dea32f402e5685ff3454b5d3a
Jan Galinski <jan.galinski@holisticon.de>
2012-10-26 13:50:51 +0200

    InjectionProvider now extends InjectionVerfier, so verfiy() is defined only once

--- src/main/java/de/akquinet/jbosscc/needle/injection/InjectionVerifier.java
+++ src/main/java/de/akquinet/jbosscc/needle/injection/InjectionVerifier.java
@@ -1,5 +1,13 @@
 package de.akquinet.jbosscc.needle.injection;
 
 public interface InjectionVerifier {
+
+	/**
+	 * Verifies the injection target.
+	 * 
+	 * @param injectionTargetInformation
+	 *            information about the injection point
+	 * @return true, if the provided object is injectable to the given injection information, otherwise false.
+	 */
 	boolean verify(InjectionTargetInformation injectionTargetInformation);
 }
