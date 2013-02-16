f8e0cfd4e4ef0e448d05300f350b230f8d5fc035
Jan Galinski <jan.galinski@holisticon.de>
2013-02-16 23:49:28 +0100

    NeedleConfiguration.get()

--- src/test/java/de/akquinet/jbosscc/needle/configuration/NeedleConfigurationTest.java
+++ src/test/java/de/akquinet/jbosscc/needle/configuration/NeedleConfigurationTest.java
@@ -2,8 +2,8 @@ package de.akquinet.jbosscc.needle.configuration;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import java.lang.annotation.Annotation;
 
+import java.lang.annotation.Annotation;
 import java.util.Set;
 
 import org.junit.Test;
@@ -15,19 +15,21 @@ import de.akquinet.jbosscc.needle.mock.EasyMockProvider;
 
 public class NeedleConfigurationTest {
 
+    private final NeedleConfiguration needleConfiguration = NeedleConfiguration.get();
+
     @Test
     public void testGetMockProviderClass_Default() throws Exception {
-        assertEquals(EasyMockProvider.class.getName(), NeedleConfiguration.getMockProviderClassName());
+        assertEquals(EasyMockProvider.class.getName(), needleConfiguration.getMockProviderClassName());
     }
 
     @Test
     public void testDBOperationClassName_NoDefaults() throws Exception {
-        assertEquals(HSQLDeleteOperation.class.getName(), NeedleConfiguration.getDBOperationClassName());
+        assertEquals(HSQLDeleteOperation.class.getName(), needleConfiguration.getDBOperationClassName());
     }
 
     @Test
     public void testGetCustomInjectionAnnotations() throws Exception {
-        Set<Class<Annotation>> customInjectionAnnotations = NeedleConfiguration.getCustomInjectionAnnotations();
+        final Set<Class<Annotation>> customInjectionAnnotations = needleConfiguration.getCustomInjectionAnnotations();
         assertEquals(2, customInjectionAnnotations.size());
         assertTrue(customInjectionAnnotations.contains(CustomInjectionAnnotation1.class));
         assertTrue(customInjectionAnnotations.contains(CustomInjectionAnnotation2.class));
