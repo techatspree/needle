1acd05419b7d86b81e5fbcddabc00bab80d9d5bf
Jan Galinski <jan.galinski@holisticon.de>
2013-02-16 23:55:38 +0100

    test spy injection

--- src/test/java/de/akquinet/jbosscc/needle/mock/MockitoProviderTest.java
+++ src/test/java/de/akquinet/jbosscc/needle/mock/MockitoProviderTest.java
@@ -1,39 +1,78 @@
 package de.akquinet.jbosscc.needle.mock;
 
-import java.util.Map;
-
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class MockitoProviderTest {
 
-	private MockitoProvider mockitoProvider = new MockitoProvider();
+    private final MockitoProvider mockitoProvider = new MockitoProvider();
 
-	@Test
-	public void testCreateMockComponent() throws Exception {
-		@SuppressWarnings("unchecked")
-		Map<String, String> mapMock = mockitoProvider.createMockComponent(Map.class);
+    @Test
+    public void shouldCreateMockComponent() throws Exception {
+        @SuppressWarnings("unchecked")
+        final Map<String, String> mapMock = mockitoProvider.createMockComponent(Map.class);
 
-		String key = "key";
-		String value = "value";
+        final String key = "key";
+        final String value = "value";
 
-		Mockito.when(mapMock.get(key)).thenReturn(value);
+        Mockito.when(mapMock.get(key)).thenReturn(value);
 
-		assertEquals(value, mapMock.get(key));
+        assertEquals(value, mapMock.get(key));
 
-	}
+    }
 
-	@Test
-	public void testCreateMockComponent_Final() throws Exception {
-		assertNull(mockitoProvider.createMockComponent(String.class));
-	}
+    @Test
+    public void shouldCreateSpyComponent() throws Exception {
+        Map<String, String> mapSpy = new HashMap<String, String>();
+        mapSpy = mockitoProvider.createSpyComponent(mapSpy);
 
-	@Test
-	public void testCreateMockComponent_isPrimitive() throws Exception {
-		assertNull(mockitoProvider.createMockComponent(int.class));
-	}
+        mapSpy.put("foo", "a");
+
+        when(mapSpy.get("bar")).thenReturn("b");
+
+        assertThat(mapSpy.get("foo"), is("a"));
+        assertThat(mapSpy.get("bar"), is("b"));
+
+        verify(mapSpy).get("foo");
+        verify(mapSpy).get("bar");
+        verify(mapSpy).put("foo", "a");
+        verifyNoMoreInteractions(mapSpy);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailToCreateSpyWhenInstanceIsNull() throws Exception {
+        mockitoProvider.createSpyComponent(null);
+    }
+
+    @Test
+    public void shouldSkipCreateMockComponentForFinalType() throws Exception {
+        assertNull(mockitoProvider.createMockComponent(String.class));
+    }
+
+    @Test
+    public void shouldSkipCreateMockComponentForPrimitiveType() throws Exception {
+        assertNull(mockitoProvider.createMockComponent(int.class));
+    }
+
+    @Test
+    public void shouldSkipCreateSpyComponentForFinal() throws Exception {
+        assertNull(mockitoProvider.createSpyComponent("foo"));
+    }
+
+    @Test
+    public void shouldSkipCreateSpyComponentForPrimitive() throws Exception {
+        assertNull(mockitoProvider.createSpyComponent(1));
+    }
 
 }
